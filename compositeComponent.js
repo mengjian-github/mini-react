beb5e2a722f4b18ae7da15d30c27643f26e86839
erasermeng(孟健) <erasermeng@tencent.com>
2022-08-31 08:41:41 +0800

    feat: compositeComponent的挂载

--- compositeComponent.js
+++ compositeComponent.js
@@ -1,3 +1,4 @@
+import { instantiate } from "./instantiate";
 export default class CompositeComponent {
     constructor(element) {
         this.element = element;
@@ -7,11 +8,25 @@ export default class CompositeComponent {
 
     mount() {
         this.instantiate();
+        this.render();
+        // 递归执行mount
+        if (this.renderedElement) {
+            return instantiate(this.renderedElement).mount();
+        }
+        return null;
+    }
+
+    render() {
+        if (this.instance) {
+            this.renderedElement = this.instance.render();
+        } else {
+            this.renderedElement = this.component(this.props);
+        }
     }
 
     instantiate() {
         if (this.component.isClassComponent) {
-            this.instance = new this.component(props);
+            this.instance = new this.component(this.props);
         } else {
             this.instance = null;   // 函数组件不需要实例化
         }
--- domComponent.js
+++ domComponent.js
@@ -2,4 +2,8 @@ export default class DomComponent {
     constructor(element) {
         this.element = element;
     }
+
+    mount() {
+        console.log('dom mount开始执行');
+    }
 }
