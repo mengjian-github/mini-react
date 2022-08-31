45e80d44e286c16386b88d0def1724c7fdfa9fa9
erasermeng(孟健) <erasermeng@tencent.com>
2022-08-31 08:58:09 +0800

    feat: 设置dom属性

--- domComponent.js
+++ domComponent.js
@@ -1,9 +1,31 @@
 export default class DomComponent {
     constructor(element) {
         this.element = element;
+        this.tag = element.type;
+        this.props = element.props;
     }
 
     mount() {
-        console.log('dom mount开始执行');
+        this.createElement();
+        this.setAttribute();
+        
+        console.log(this.node);
+        return this.node;
+    }
+
+    createElement() {
+        this.node = document.createElement(this.tag);
+    }
+
+    setAttribute() {
+        Object.keys(this.props).forEach(attribute => {
+            if (attribute !== 'children') {
+                if (attribute === 'className') {
+                    this.node.setAttribute('class', this.props[attribute])
+                } else {
+                    this.node.setAttribute(attribute, this.props[attribute]);
+                }
+            }
+        })
     }
 }
