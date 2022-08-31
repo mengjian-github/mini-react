c136bc69c57d542407c7d6f0d6fff84d682d6a5d
erasermeng(孟健) <erasermeng@tencent.com>
2022-08-31 17:31:08 +0800

    feat: 支持生命周期

--- compositeComponent.js
+++ compositeComponent.js
@@ -7,12 +7,19 @@ export default class CompositeComponent {
     this.props = element.props;
   }
 
+  execHook(name, ...args) {
+    if (this.instance?.[name]) {
+        this.instance[name].call(this.instance, ...args);
+    }
+  }
+
   getHostNode() {
     return this.renderedComponent?.getHostNode();
   }
 
   mount() {
     this.instantiate();
+    this.execHook('componentWillMount');
     this.render();
 
     return this.toMount();
@@ -20,32 +27,44 @@ export default class CompositeComponent {
 
   toMount() {
     // 递归执行mount
+    let result = null;
     if (this.renderedElement) {
       this.renderedComponent = instantiate(this.renderedElement);
-      return this.renderedComponent.mount();
+      result = this.renderedComponent.mount();
     }
-    return null;
+
+    this.execHook('componentDidMount');
+    return result;
   }
 
 
   receive(nextElement) {
+    this.execHook('componetWillReceiveProps', nextElement.props);
+    const prevProps = this.props;
+
     this.element = nextElement;
     this.component = nextElement.type;
     this.props = nextElement.props;
     this.instance.props = this.props; // 更新组件的props
 
-    this.update(); // 递归执行子组件更新
+    this.update({}, prevProps); // 递归执行子组件更新
+    this.execHook('componentDidUpdate');
   }
 
-  update(state) {
+  update(state, prevProps = this.props) {
+    const prevState = this.instance.state;
+    const nextState = { ...this.instance.state, ...state };
+    this.execHook('componentWillUpdate', this.props, nextState);
+
     if (state) {
       // 更新state
-      this.instance.state = { ...this.instance.state, ...state };
+      this.instance.state = nextState;
     }
 
     const prevElement = this.renderedElement;
     this.render();
     const nextElement = this.renderedElement;
+
 
     if (prevElement.type === nextElement.type) {
       // 可以进行增量更新
@@ -58,9 +77,12 @@ export default class CompositeComponent {
       // 替换DOM节点（这里简便起见将更新DOM操作写在这里，理论上React组件和平台无关，应该依赖注入）
       hostNode.parentNode.replaceChild(newNode, hostNode);
     }
+
+    this.execHook('componentDidUpdate', prevProps, prevState);
   }
 
   unmount() {
+    this.execHook('componentWillUnmount');
     this.renderedComponent?.unmount();
   }
 
